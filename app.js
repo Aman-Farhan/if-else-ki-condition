956b6c2fa5b9e4a5a90306a0fa55fec0e78c6102
Aman <amanfarhan2008@gmail.com>
2025-07-09 23:49:28 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// let userInput = +prompt("enter a num ");
+// let userInput1 = +prompt("enter a num ");
+// let userInput2 = prompt("enter a num ");
+
+// if ()
+
+
+
+
+let num1 = parseFloat(prompt("Pehla number daalo:"));
+let num2 = parseFloat(prompt("Dosra number daalo:"));
+let operator = prompt("Kya karna hai? +, -, *, /");
+
+let result;
+
+if (operator === "+") {
+  result = num1 + num2;
+} else if (operator === "-") {
+  result = num1 - num2;
+} else if (operator === "*") {
+  result = num1 * num2;
+} else if (operator === "/") {
+  if (num2 !== 0) {
+    result = num1 / num2;
+  } else {
+    result = "0 se divide nahi kar sakte!";
+  }
+} else {
+  result = "Invalid operator dala hai!";
+}
+
+alert("Result: " + result);
